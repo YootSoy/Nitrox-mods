12b89f4a537527f0cddd74017563cb7a793e792b
yoot <turkishpatriotman2@gmail.com>
2026-05-08 20:29:42 +0300

    Update uGUI_TabbedControlsPanel_AddBindingOption_Patch.cs

--- NitroxPatcher/Patches/Persistent/uGUI_TabbedControlsPanel_AddBindingOption_Patch.cs
+++ NitroxPatcher/Patches/Persistent/uGUI_TabbedControlsPanel_AddBindingOption_Patch.cs
@@ -3,24 +3,27 @@ using System.Reflection;
 namespace NitroxPatcher.Patches.Persistent;
 
 /// <summary>
-/// Intercepts binding options created by Nitrox to remove the "Option" prefix given by <see cref="GameInputSystem.PopulateBindingSettings"/>
+/// Intercepts binding options created by Nitrox to remove the "Option" prefix.
+/// Avoids recursive AddBindingOption calls which can break rebinding.
 /// </summary>
 public partial class uGUI_TabbedControlsPanel_AddBindingOption_Patch : NitroxPatch, IPersistentPatch
 {
-    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((uGUI_TabbedControlsPanel t) => t.AddBindingOption(default, default, default, default));
+    private static readonly MethodInfo TARGET_METHOD =
+        Reflect.Method((uGUI_TabbedControlsPanel t) => t.AddBindingOption(default, default, default, default));
 
     private const string SUBNAUTICA_OPTION_PREFIX = "Option";
     private const string DETECT_OPTION_PREFIX = "OptionNitrox";
 
-    public static bool Prefix(uGUI_TabbedControlsPanel __instance, int tabIndex, string label, GameInput.Device device, GameInput.Button button, ref uGUI_Bindings __result)
+    public static void Prefix(ref string label)
     {
-        if (label.StartsWith(DETECT_OPTION_PREFIX))
+        if (string.IsNullOrEmpty(label))
         {
-            // Cut "Option" from the label
-            __result = __instance.AddBindingOption(tabIndex, label[SUBNAUTICA_OPTION_PREFIX.Length..], device, button);
-            return false;
+            return;
         }
 
-        return true;
+        if (label.StartsWith(DETECT_OPTION_PREFIX))
+        {
+            label = label[SUBNAUTICA_OPTION_PREFIX.Length..];
+        }
     }
 }
