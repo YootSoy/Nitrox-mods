6d2833bfe888385fd381dae306d235e141144971
yoot <turkishpatriotman2@gmail.com>
2026-05-08 20:28:23 +0300

    Update GameInputSystem_Initialize_Patch.cs

--- NitroxPatcher/Patches/Persistent/GameInputSystem_Initialize_Patch.cs
+++ NitroxPatcher/Patches/Persistent/GameInputSystem_Initialize_Patch.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
@@ -9,66 +11,167 @@ using UnityEngine.InputSystem;
 namespace NitroxPatcher.Patches.Persistent;
 
 /// <summary>
-/// Inserts Nitrox's keybinds in the new Subnautica input system
+/// Inserts Nitrox's keybinds in the new Subnautica input system.
+/// Also makes Nitrox keybinds safer when Nautilus/BepInEx is loaded.
 /// </summary>
 public partial class GameInputSystem_Initialize_Patch : NitroxPatch, IPersistentPatch
 {
-    private static readonly MethodInfo TARGET_METHOD = Reflect.Method((GameInputSystem t) => t.Initialize());
+    private static readonly MethodInfo TARGET_METHOD =
+        Reflect.Method((GameInputSystem t) => t.Initialize());
+
+    private static readonly MethodInfo DEINITIALIZE_METHOD =
+        Reflect.Method((GameInputSystem t) => t.Deinitialize());
+
+    private static GameInput.Button[]? oldAllActions;
+
+    public override void Patch(Harmony harmony)
+    {
+        PatchPrefix(harmony, TARGET_METHOD, ((Action<GameInputSystem>)Prefix).Method);
+        PatchTranspiler(harmony, TARGET_METHOD, ((Func<IEnumerable<CodeInstruction>, IEnumerable<CodeInstruction>>)Transpiler).Method);
+        PatchPrefix(harmony, DEINITIALIZE_METHOD, ((Action)DeinitializePrefix).Method);
+    }
 
     public static void Prefix(GameInputSystem __instance)
     {
         CachedEnumString<GameInput.Button> actionNames = GameInput.ActionNames;
 
         int buttonId = KeyBindingManager.NITROX_BASE_ID;
+
+        oldAllActions ??= GameInput.AllActions;
+
+        FieldInfo? allActionsField = typeof(GameInput).GetField(
+            nameof(GameInput.AllActions),
+            BindingFlags.Public | BindingFlags.Static
+        );
+
+        if (allActionsField != null && GameInput.AllActions != null)
+        {
+            GameInput.Button[] nitroxButtons = Enumerable
+                .Range(KeyBindingManager.NITROX_BASE_ID, KeyBindingManager.KeyBindings.Count)
+                .Cast<GameInput.Button>()
+                .ToArray();
+
+            GameInput.Button[] newAllActions = GameInput.AllActions
+                .Concat(nitroxButtons)
+                .Distinct()
+                .ToArray();
+
+            allActionsField.SetValue(null, newAllActions);
+        }
+
         foreach (KeyBinding keyBinding in KeyBindingManager.KeyBindings)
         {
             GameInput.Button button = (GameInput.Button)buttonId++;
+
             actionNames.valueToString[button] = keyBinding.ButtonLabel;
 
-            if (!string.IsNullOrEmpty(keyBinding.DefaultKeyboardKey))
+            RegisterLanguageOption(button, keyBinding.ButtonLabel);
+
+            if (!string.IsNullOrEmpty(keyBinding.DefaultKeyboardKey) &&
+                !GameInputSystem.bindingsKeyboard.ContainsKey(button))
             {
-                // See GameInputSystem.bindingsKeyboard definition
-                GameInputSystem.bindingsKeyboard.Add(button, $"<Keyboard>/{keyBinding.DefaultKeyboardKey}");
+                GameInputSystem.bindingsKeyboard.Add(button, $"/{keyBinding.DefaultKeyboardKey}");
             }
-            if (!string.IsNullOrEmpty(keyBinding.DefaultControllerKey))
+
+            if (!string.IsNullOrEmpty(keyBinding.DefaultControllerKey) &&
+                !GameInputSystem.bindingsController.ContainsKey(button))
             {
-                // See GameInputSystem.bindingsController definition
-                GameInputSystem.bindingsController.Add(button, $"<Gamepad>/{keyBinding.DefaultControllerKey}");
+                GameInputSystem.bindingsController.Add(button, $"/{keyBinding.DefaultControllerKey}");
             }
         }
     }
 
-    /*
-     * Modifying the actions must happen before actionMapGameplay.Enable because that line is responsible
-     * for activating the actions callback we'll be setting
-     * 
-     * GameInputSystem_Initialize_Patch.RegisterKeybindsActions(this); <--- [INSERTED LINE]
-     * this.actionMapGameplay.Enable();
-     */
+    public static void DeinitializePrefix()
+    {
+        if (oldAllActions == null)
+        {
+            return;
+        }
+
+        FieldInfo? allActionsField = typeof(GameInput).GetField(
+            nameof(GameInput.AllActions),
+            BindingFlags.Public | BindingFlags.Static
+        );
+
+        allActionsField?.SetValue(null, oldAllActions);
+        oldAllActions = null;
+    }
+
+
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        return new CodeMatcher(instructions).MatchStartForward([
-                                                new(OpCodes.Ldarg_0),
-                                                new(OpCodes.Ldfld),
-                                                new(OpCodes.Callvirt, Reflect.Method((InputActionMap t) => t.Enable()))
-                                            ])
-                                            .Insert([
-                                                new CodeInstruction(OpCodes.Ldarg_0),
-                                                new CodeInstruction(OpCodes.Call, Reflect.Method(() => RegisterKeybindsActions(default)))
-                                            ])
-                                            .InstructionEnumeration();
+        return new CodeMatcher(instructions)
+            .MatchStartForward(
+            [
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldfld),
+                new(OpCodes.Callvirt, Reflect.Method((InputActionMap t) => t.Enable()))
+            ])
+            .Insert(
+            [
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, Reflect.Method(() => RegisterKeybindsActions(default!)))
+            ])
+            .InstructionEnumeration();
     }
-    
+
     /// <summary>
-    /// Set the actions callbacks for our own keybindings once they're actually created only
+    /// Sets callbacks for Nitrox keybinds.
+    /// If Nautilus or another input patch changes initialization order, Nitrox creates missing InputAction entries itself.
     /// </summary>
     public static void RegisterKeybindsActions(GameInputSystem gameInputSystem)
     {
         int buttonId = KeyBindingManager.NITROX_BASE_ID;
+
         foreach (KeyBinding keyBinding in KeyBindingManager.KeyBindings)
         {
             GameInput.Button button = (GameInput.Button)buttonId++;
-            gameInputSystem.actions[button].started += keyBinding.Execute;
+
+            if (!gameInputSystem.actions.TryGetValue(button, out InputAction action))
+            {
+                string buttonName = GameInput.ActionNames.valueToString.TryGetValue(button, out string name)
+                    ? name
+                    : button.ToString();
+
+                action = new InputAction(buttonName, InputActionType.Button);
+
+                if (!string.IsNullOrEmpty(keyBinding.DefaultKeyboardKey))
+                {
+                    action.AddBinding($"<Keyboard>/{keyBinding.DefaultKeyboardKey}");
+                }
+
+                if (!string.IsNullOrEmpty(keyBinding.DefaultControllerKey))
+                {
+                    action.AddBinding($"<Gamepad>/{keyBinding.DefaultControllerKey}");
+                }
+
+                gameInputSystem.actions[button] = action;
+                action.started += gameInputSystem.OnActionStarted;
+                action.Enable();
+            }
+
+            action.started -= keyBinding.Execute;
+            action.started += keyBinding.Execute;
+        }
+    }
+
+    private static void RegisterLanguageOption(GameInput.Button button, string label)
+    {
+        if (Language.main == null || string.IsNullOrEmpty(label))
+        {
+            return;
+        }
+
+        string key = $"Option{(int)button}";
+
+        FieldInfo? stringsField = typeof(Language).GetField(
+            "strings",
+            BindingFlags.Instance | BindingFlags.NonPublic
+        );
+
+        if (stringsField?.GetValue(Language.main) is Dictionary<string, string> strings)
+        {
+            strings[key] = label;
         }
     }
 }
